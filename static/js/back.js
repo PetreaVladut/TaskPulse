9e094459c1b8d1eabe00a027cd6d3b471c37d5d7
PetreaVladut <93314910+PetreaVladut@users.noreply.github.com>
2023-12-05 15:11:27 +0200

    Add files via upload

--- static/js/back.js
+++ static/js/back.js
@@ -1,65 +1,128 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', function () {
+    // Select all the project columns as drag-and-drop containers
+    var containers = Array.from(document.querySelectorAll('.project-column'));
 
-    var dragSrcEl = null;
-    
-    function handleDragStart(e) {
-      this.style.opacity = '0.1';
-      this.style.border = '3px dashed #c4cad3';
-      
-      dragSrcEl = this;
+    // Initialize Dragula with these containers
+    var drake = dragula(containers);
+
+    drake.on('drop', function (el, target, source, sibling) {
+        // el: the element being dragged
+        // target: the target container the element was dropped into
+        // source: the original container the element was dragged from
+        // sibling: the element next to the dragged element after dropping
+
+        // Example: Update task status on server using AJAX (fetch)
+        let taskId = el.getAttribute('data-task-id');
+        let newStatus = target.getAttribute('data-status');  // Ensure your columns have 'data-status' attribute
+
+        fetch('/update-task-status', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ task_id: taskId, new_status: newStatus }),
+        })
+        .then(response => response.json())
+        .then(data => console.log(data));
+    });
+});
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const addTaskButtons = document.querySelectorAll(".add-task-button");
   
-      e.dataTransfer.effectAllowed = 'move';
-      e.dataTransfer.setData('text/html', this.innerHTML);
+    // Function to handle button click
+    function handleAddTaskClick() {
+      // You can add your logic here
+      alert("Add Task button clicked!");
     }
   
-    function handleDragOver(e) {
-      if (e.preventDefault) {
-        e.preventDefault();
-      }
-  
-      e.dataTransfer.dropEffect = 'move';
-      
-      return false;
-    }
-  
-    function handleDragEnter(e) {
-      this.classList.add('task-hover');
-    }
-  
-    function handleDragLeave(e) {
-      this.classList.remove('task-hover');
-    }
-  
-    function handleDrop(e) {
-      if (e.stopPropagation) {
-        e.stopPropagation(); // stops the browser from redirecting.
-      }
-      
-      if (dragSrcEl != this) {
-        dragSrcEl.innerHTML = this.innerHTML;
-        this.innerHTML = e.dataTransfer.getData('text/html');
-      }
-      
-      return false;
-    }
-  
-    function handleDragEnd(e) {
-      this.style.opacity = '1';
-      this.style.border = 0;
-      
-      items.forEach(function (item) {
-        item.classList.remove('task-hover');
-      });
-    }
-    
-    
-    let items = document.querySelectorAll('.task'); 
-    items.forEach(function(item) {
-      item.addEventListener('dragstart', handleDragStart, false);
-      item.addEventListener('dragenter', handleDragEnter, false);
-      item.addEventListener('dragover', handleDragOver, false);
-      item.addEventListener('dragleave', handleDragLeave, false);
-      item.addEventListener('drop', handleDrop, false);
-      item.addEventListener('dragend', handleDragEnd, false);
+    // Add click event listener to each button
+    addTaskButtons.forEach(function (button) {
+      button.addEventListener("click", handleAddTaskClick);
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Example: Alert when form is submitted
+    document.querySelector('form').onsubmit = function() {
+        alert('Task is being added!');
+    };
+});
+function updateTags() {
+  var input = document.getElementById('tags');
+  var container = document.getElementById('tags-container');
+  container.innerHTML = '';  // Clear existing tags
+
+  // Split the input value by commas and filter out empty values
+  var tags = input.value.split(',').filter(function(tag) {
+      return tag.trim() !== '';
+  });
+
+  // Create a span element for each tag
+  tags.forEach(function(tag) {
+      var span = document.createElement('span');
+      span.textContent = tag.trim();
+      span.style.backgroundColor = '#50b3a2';  // Tag background color
+      span.style.color = 'white';              // Tag text color
+      span.style.padding = '5px 10px';
+      span.style.marginRight = '5px';
+      span.style.borderRadius = '5px';
+      container.appendChild(span);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.assign-button').forEach(button => {
+      button.addEventListener('click', function() {
+          let taskId = this.dataset.taskId;
+          let newUserId = prompt("Enter new user ID for task " + taskId); // Simple prompt, replace with your own UI
+
+          if(newUserId) {
+              fetch('/update-assignee', {
+                  method: 'POST',
+                  headers: {
+                      'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({ task_id: taskId, new_user_id: newUserId }),
+              })
+              .then(response => response.json())
+              .then(data => {
+                  if(data.status === 'success') {
+                      alert('Assignee updated successfully');
+                      // Update the UI here if necessary
+                  } else {
+                      alert('Error updating assignee');
+                  }
+              });
+          }
+      });
+  });
+});
+
+function editFunction() {
+    console.log("Edit button clicked");
+    // Add your edit logic here
+}
+
+function deleteFunction() {
+    console.log("Delete button clicked");
+    // Add your delete logic here
+}
+
+// Optional: close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+document.querySelector('.dropdown').addEventListener('click', function() {
+    document.querySelector('.dropdown-content').classList.toggle('show');
+});
